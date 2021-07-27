eb28f475f48848e412a064dcd3df2c9ee8874fdc
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-07-27 12:51:59 -0300

    Conditions

---
+++ aula11/ex09.js
@@ -0,0 +1,7 @@
+var velocity = 6
+
+console.log(`A velocidade do seu carro é de ${velocity}Km/h`)
+if(velocity > 60) {
+  console.log(`Você ultrapassou a velocidade permitida.`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex10.js
@@ -0,0 +1,7 @@
+var country = 'EUA'
+console.log(`Vivendo em ${country}`)
+if(country == 'Brasil') {
+  console.log('Você é Brasileiro')
+} else {
+  console.log('Você é Estrangeiro')
+}
