fdd198bff8487418ab2c1bcb878d59241a8cea36
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-08-03 00:48:42 -0300

    for

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,11 +1,10 @@
-/* 
-estrutura de teste lógico no começo
 let c = 1
+/* 
+// estrutura de teste lógico no começo while
 while (c <= 6) {
   console.log(`Passo ${c}`)
   c++
 }
-*/
 
 // estrutura de teste lógico no final
 let c = 1
@@ -13,3 +12,13 @@ do {
   console.log(`Passo ${c}`)
   c++
 } while  (c <= 6)
+*/
+
+while (c <= 5) {
+  console.log(`Passo ${c}`)
+  c++
+}
+// (inicio ; teste ; incremento)
+for (let c = 1 ; c <= 3 ; c++){
+  console.log('oi')
+}
